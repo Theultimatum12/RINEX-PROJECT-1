a41467a70cd71891badc8cc0d4ab26e5e6fcc32d
Theultimatum12 <91970584+Theultimatum12@users.noreply.github.com>
2022-08-10 23:29:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import joblib
-model=joblib.load('Fuel-Detector')
-st.title('Fuel Detection')
-ip=st.text_input('Enter the fuel type')
+model = joblib.load('Fuel-Detector')
+st.title('Fuel-Detection_Portal')
+ip=st.text_input('Enter the Fuel Used')
 op=model.predict([ip])
 if st.button('Predict')
   st.title(op[0])
